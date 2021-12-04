ee83cff6be72323a1838f05511fb647b131a0ce3
신영민 <rightones@outlook.com>
2021-12-05 03:30:45 +0900

    .

--- recommend/serializers.py
+++ recommend/serializers.py
@@ -34,16 +34,8 @@ class InterestTopicUpdateSerializer(serializers.ModelSerializer):
         model = InterestTopicProfile
         fields="__all__"
 
-    def create(self, validated_data):
-        user = self.context["request"].user
-        return InterestTopicProfile(user=user, **validated_data)
-
 
 class AdeptTopicUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = AdeptTopicProfile
         fields="__all__"
-
-    def create(self, validated_data):
-        user = self.context["request"].user
-        return AdeptTopicProfile(user=user, **validated_data)
