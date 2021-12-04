c5fed44e880d9b4564aec7bc8c4c49063e3e3436
신영민 <rightones@outlook.com>
2021-12-04 22:32:21 +0900

    .

--- team/views.py
+++ team/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django_filters import filters
+from rest_framework import filters
 from rest_framework import viewsets
 
 from .models import Team
