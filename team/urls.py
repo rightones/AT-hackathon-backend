61f80e3106802fbe6acd62c7b1250c565742d90b
신영민 <rightones@outlook.com>
2021-12-05 03:43:24 +0900

    .

--- team/urls.py
+++ team/urls.py
@@ -4,5 +4,6 @@ from .views import TeamViewSet, TeamPositionViewSet
 
 urlpatterns = [
     path("", TeamViewSet.as_view({"get": "list", "post":"create"})),
+    path("/<int:pk>", TeamViewSet.as_view({"get": "retrieve"})),
     path("/positions", TeamPositionViewSet.as_view({"get": "list", "post":"create"})),
 ]
