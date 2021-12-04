51b82818c94053ad514c4d7084768713ef1482f9
신영민 <rightones@outlook.com>
2021-12-05 03:02:41 +0900

    .

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DJANGO_DEBUG", False))
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
