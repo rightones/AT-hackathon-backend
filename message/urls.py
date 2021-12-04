b5a046eef7b227cec8276a3b4e1f6d3dc97030e8
신영민 <rightones@outlook.com>
2021-12-05 07:05:32 +0900

    .

--- message/urls.py
+++ message/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import MessageViewSet
 
 urlpatterns = [
-    path("", MessageViewSet.as_view({"get": "list"})),
+    path("", MessageViewSet.as_view({"get": "list", "post":"create"})),
     path("/<int:pk>", MessageViewSet.as_view({"get": "retrieve", "post":"accept"})),
 ]
